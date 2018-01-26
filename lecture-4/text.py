e0fabe626a412a47aa9ad0301a56f7151bf5c98d
Pedro Delfino <pdelfino@users.noreply.github.com>
2018-01-25 23:56:43 -0200

    Update text.py

--- lecture-4/text.py
+++ lecture-4/text.py
@@ -1,16 +1,16 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACe7d94a4fdbf27e5f904e213207b4dbdb"
+account_sid = ""
 
 # Your Auth Token from twilio.com/console
-auth_token  = "32f56018ad6a94749c0bc6bc3eb535b6"
+auth_token  = ""
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
-    to="+31993547769", 
-    from_="+552731800439",
+    to="+", 
+    from_="+",
     body="Hello from Python!")
 
 print (message.sid)
