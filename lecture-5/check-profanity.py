1276be06d05c859ba2c6fe59af2df9ea404e7008
Pedro Delfino <p.delfino01@gmail.com>
2018-01-27 14:01:03 -0200

    do lecture-5 exercises

--- lecture-5/check-profanity.py
+++ lecture-5/check-profanity.py
@@ -1,4 +1,40 @@
+import urllib
+
 def read_text():
-    open("movie_quotes.txt")
+
+    quotes = open("/home/pedro/udacity-py/lecture-5/movie_quotes.txt")
     
-read_text()
+    words = [] 
+
+    for line in quotes:
+            
+        words_line = line.split()
+    
+        words = words + words_line
+    
+    #print (words)
+
+    for word in words:
+        
+        #print (word) 
+
+        connection = urllib.urlopen("http://www.wdylike.appspot.com/?q="+str(word))
+
+        outcome = connection.read()
+        
+        #print (outcome)
+
+        if outcome=="true":
+           
+           return "Profatiny detected cursed word: "+word
+        
+        # importante fechar a connection, se n fica rodando, tipo num loop sem fim
+        connection.close()
+
+
+    else:
+        
+        return "Text is clean"
+
+print (read_text())
+
